3028dcbe05275b1bb661864330e277f0a3c238bb
duhao <duhao@ucnology.com>
2020-05-27 13:14:12 +0800

    remove config settings

--- userData.js
+++ userData.js
@@ -8,9 +8,9 @@ module.exports = {
     // 口罩数量 固定5 切勿更改
     reservationNumber: "5",
     // 领取口罩的药店名称 该信息可查阅  README.md -> 使用帮助 -> 第三条
-    pharmacyName: "国胜大药房坝下路店",
+    pharmacyName: "百姓缘大药房万达店",
     // 领取口罩的药店编号ID 该信息可查阅  README.md -> 使用帮助 -> 第三条
-    pharmacyCode: "11183",
+    pharmacyCode: "10240",
     // 通过上次请求获取到的有效的口罩信息 数量和时间点 请勿填写，自动获取生成
     pharmacyPhase: "",
     // 请勿填写，自动获取生成
